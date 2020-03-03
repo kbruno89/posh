7adc467b65d9d7dae07de9c1fa26beb8f9eae06f
Bruno Kammers <bkammers@gmail.com>
2020-03-03 13:25:28 -0300

    Update fl_nlb.ps1

--- fl_nlb.ps1
+++ fl_nlb.ps1
@@ -13,22 +13,22 @@ $DOMAIN = ''
 $ADLOCAL = ''
 $DNSA = ''
 $DNSB = ''
+...
 $DNSX = ''
 
 ########## UNDER CONTRUCTION ...
 
 
-echo "PASSO 1 - Consulta"
+echo "Get/Consulta"
 Get-DnsServerResourceRecord -ComputerName '$ADLOCAL' -ZoneName '$DOMAIN' -RRType A -ErrorAction:Ignore | Where-Object Hostname -eq '$DNSX'
 
 
-echo "PASSO 2 - Insere"
+echo "Delete/Remove"
+Remove-DnsServerResourceRecord -Name '$DNSX' -ZoneName '$DOMAIN' -RRType A
+
+
+echo "Insert/Insere"
 Add-DnsServerResourceRecordA -Name '$DNSX' -ZoneName '$DOMAIN' -IPv4Address 192.168.152.56
 
 
-echo "PASSO 3 - Consulta"
-Get-DnsServerResourceRecord -ComputerName '$ADLOCAL' -ZoneName '$DOMAIN' -RRType A -ErrorAction:Ignore | Where-Object Hostname -eq '$DNSX'
 
-
-echo "PASSO 4 - Remove"
-Remove-DnsServerResourceRecord -Name '$DNSX' -ZoneName '$DOMAIN' -RRType A
