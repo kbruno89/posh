ef8db74f2654d746adaa59a2926db372e1ec70fb
Bruno Kammers <bkammers@gmail.com>
2020-03-13 17:23:48 -0300

    Update fl_nlb.ps1

--- fl_nlb.ps1
+++ fl_nlb.ps1
@@ -9,28 +9,29 @@
 
 cls
 
-$DOMAIN = ''
-$ADLOCAL = ''
-$DNSA = ''
-$DNSB = ''
+$DOMAIN = 'contoso.com'
+$ADLOCAL = 'ad01.contoso.com'
+$EXT = '8.8.8.8'
+$DNS = 'xyz.com'
+$IPA = '1.1.1.1'
 ...
-$DNSX = ''
+$IPX = '2.2.2.2'
 
 ########## UNDER CONSTRUCTION ...
 
 echo "External Validation/Consulta Externa"
-$VAR = (nslookup "$DNSA" 8.8.8.8 2> $null | findstr "Address" | where { $_ -notmatch "8.8.8.8" }) | ForEach-Object { $_.split(":")[1] } | ForEach-Object { $_.split(" ")[2] }
+$VAR = (nslookup "$DNS" "$EXT" 2> $null | findstr "Address" | where { $_ -notmatch "$EXT" }) | ForEach-Object { $_.split(":")[1] } | ForEach-Object { $_.split(" ")[2] }
 $VAR
 echo "" ; echo ""
 
 
 echo "Get/Consulta"
-Get-DnsServerResourceRecord -ComputerName '$ADLOCAL' -ZoneName '$DOMAIN' -RRType A -ErrorAction:Ignore | Where-Object Hostname -eq '$DNSX'
+Get-DnsServerResourceRecord -ComputerName '$ADLOCAL' -ZoneName '$DOMAIN' -RRType A -ErrorAction:Ignore | Where-Object Hostname -eq '$DNS'
 
 
 echo "Delete/Remove"
-Remove-DnsServerResourceRecord -Name '$DNSX' -ZoneName '$DOMAIN' -RRType A
+Remove-DnsServerResourceRecord -Name '$DNS' -ZoneName '$DOMAIN' -RRType A
 
 
 echo "Insert/Insere"
-Add-DnsServerResourceRecordA -Name '$DNSX' -ZoneName '$DOMAIN' -IPv4Address 192.168.0.X
+Add-DnsServerResourceRecordA -Name '$DNS' -ZoneName '$DOMAIN' -IPv4Address "$IP A ... OR X"
